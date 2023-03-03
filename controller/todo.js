14e47c308eb3e1da508ee8ce57a2856b823b9de0
chipmunk-dev <devquokkajeong@gmail.com>
2023-03-04 00:23:45 +0900

    feat: create todoRouter

---
+++ controller/todo.js
@@ -0,0 +1,6 @@
+import * as todoRepository from "../data/todo.js";
+
+export const createList = (reqeust, response) => {};
+export const getList = (reqeust, response) => {};
+export const updateList = (reqeust, response) => {};
+export const deleteList = (reqeust, response) => {};
