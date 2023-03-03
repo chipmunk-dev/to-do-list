ebbc7b0f57a9d261d8f79634866cb19cbc813e48
chipmunk-dev <devquokkajeong@gmail.com>
2023-03-04 00:51:58 +0900

    feat: create todo for controller & repository

--- controller/todo.js
+++ controller/todo.js
@@ -1,6 +1,16 @@
 import * as todoRepository from "../data/todo.js";
 
-export const createList = (reqeust, response) => {};
+export const createList = (reqeust, response) => {
+	const { userId, text } = reqeust.body;
+
+	try {
+		const todo = todoRepository.save({ userId, text });
+
+		return response.status(201).json(todo);
+	} catch (error) {
+		return response.status(401).json({ message: error.message });
+	}
+};
 export const getList = (reqeust, response) => {};
 export const updateList = (reqeust, response) => {};
 export const deleteList = (reqeust, response) => {};
---
+++ data/todo.js
@@ -0,0 +1,19 @@
+const data = [];
+
+export const save = data => {
+	const { userId, text } = data;
+
+	// TODO: validation 적용하기
+	if (!userId || !text) throw new Error("invalide data");
+
+	// TODO: user data 조회 후 데이터 같이 넣어주기
+	const todo = {
+		id: Date.now(),
+		userId,
+		text,
+		createdAt: new Date().toDateString(),
+	};
+	data.push(todo);
+
+	return todo;
+};
