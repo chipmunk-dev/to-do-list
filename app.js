1d0747c446b633db5e1d74f8497b50bbde3cdf23
chipmunk <107650362+chipmunk-dev@users.noreply.github.com>
2023-03-08 22:35:30 +0900

    feat save token with cookie (#19)

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 // global
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import helmet from "helmet";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 // local
-import { config } from "./config.js";
-import todoRouter from "./router/todo.js";
-import userRouter from "./router/user.js";
-import { connectDB } from "./db/db.js";
+import todoRouter from './router/todo.js';
+import userRouter from './router/user.js';
+import { config } from './config.js';
+import { connectDB } from './db/db.js';
 
 const app = express();
 const corsOptions = {
 	origin: config.cors.allowOrigin,
 	optionsSuccessStatus: 200,
+	credentials: true,
 };
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(helmet());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 // route
-app.use("/todo", todoRouter);
-app.use("/user", userRouter);
+app.use('/todo', todoRouter);
+app.use('/user', userRouter);
 
 app.use((_req, res, _next) => res.sendStatus(400));
 
@@ -33,8 +36,8 @@ app.use((error, _req, res, _next) => {
 });
 
 connectDB().then(() => {
-	console.log("Connect DB...");
+	console.log('Connect DB...');
 	app.listen(config.port, () => {
-		console.log("Connect Server...");
+		console.log('Connect Server...');
 	});
 });
