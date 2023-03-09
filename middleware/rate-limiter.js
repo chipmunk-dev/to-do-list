4acd643881f15d7ae4d9f3e4036520051adb4f78
chipmunk-dev <devquokkajeong@gmail.com>
2023-03-09 12:54:34 +0900

    feat: apply rate-limiter middleware

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -1,5 +1,5 @@
 import rateLimit from 'express-rate-limit';
-import { config } from '../config';
+import { config } from '../config.js';
 
 export default rateLimit({
 	windowMs: config.rateLimit.windowMs,
